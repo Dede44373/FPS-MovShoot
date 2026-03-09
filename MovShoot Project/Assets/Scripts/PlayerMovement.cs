e4aa7e2c557e9639236c0651afa054ea74432978
Dede44373 <ysd4417@autuni.ac.nz>
2026-03-10 05:32:28 +1300

    WASD movement
    Never forgot this dogcrap took 2 hours

--- MovShoot Project/Assets/Scripts/PlayerMovement.cs
+++ MovShoot Project/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
 
     public float groundDrag;
 
+    public UserInputs Controls;
+
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
@@ -14,10 +17,8 @@ public class PlayerMovement : MonoBehaviour
 
     public Transform orientation;
 
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
+    Vector2 moveDirection;
+    Vector3 calculatedMoveDirection;
 
     Rigidbody rb;
 
@@ -25,23 +26,54 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        Controls = UserInputManager.Instance.Controls;
+        Controls.Player.Move.performed += ChangePlayerMovement;
+        Controls.Player.Move.canceled += StopPlayerMovement;
+        Controls.Enable();
+
+    }
+    private void OnDisable()
+    {
+        Controls.Player.Move.performed -= ChangePlayerMovement;
+        Controls.Player.Move.canceled -= StopPlayerMovement;
+    }   
+    private void ChangePlayerMovement(InputAction.CallbackContext ctx)
+    {
+        moveDirection = ctx.ReadValue<Vector2>();
+        print($"Movement direction: {moveDirection}");
+
     }
 
+    private void StopPlayerMovement(InputAction.CallbackContext ctx)
+    {
+        calculatedMoveDirection = Vector3.zero;
+        moveDirection = Vector2.zero;
+
+    }
     private void FixedUpdate()
     {
         MovePlayer();
     }
 
-    private void MyInput()
+    private void Update()
     {
-        horizontalInput = input
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        Debug.DrawRay(transform.position, Vector3.down,Color.red);
+        //handle drag
+        if (grounded)   
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
     }
-
-    private void MovePlayer() 
+    void MovePlayer()
     {
-        // calculate movement direction
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        calculatedMoveDirection = orientation.forward * moveDirection.y + orientation.right * moveDirection.x;
+        rb.AddForce(calculatedMoveDirection * moveSpeed * 10f, ForceMode.Force);
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // calculatedMoveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        //  rb.AddForce(calculatedMoveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        // calculate movement direction
     }
 }
