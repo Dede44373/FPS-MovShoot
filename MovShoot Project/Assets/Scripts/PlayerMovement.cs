d6b95e257ff5d03223863b89ec9ab00a183172f0
Dede44373 <ysd4417@autuni.ac.nz>
2026-03-11 00:48:57 +1300

    trying to add jumping
    this sucks

--- MovShoot Project/Assets/Scripts/PlayerMovement.cs
+++ MovShoot Project/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,13 @@ public class PlayerMovement : MonoBehaviour
 
     public float groundDrag;
 
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump = true;
+
+    [Header("Controls")]
     public UserInputs Controls;
 
     [Header("Ground Check")]
@@ -29,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
         Controls = UserInputManager.Instance.Controls;
         Controls.Player.Move.performed += ChangePlayerMovement;
         Controls.Player.Move.canceled += StopPlayerMovement;
+        Controls.Player.Jump.performed += PlayerJump;
         Controls.Enable();
 
     }
@@ -36,6 +44,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Controls.Player.Move.performed -= ChangePlayerMovement;
         Controls.Player.Move.canceled -= StopPlayerMovement;
+        Controls.Player.Jump.performed -= PlayerJump;
     }   
     private void ChangePlayerMovement(InputAction.CallbackContext ctx)
     {
@@ -50,6 +59,21 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = Vector2.zero;
 
     }
+
+    private void PlayerJump(InputAction.CallbackContext ctx)
+    {
+        
+        if(readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+            Debug.Log("I hate this shitty input system");
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+    }
+
     private void FixedUpdate()
     {
         MovePlayer();
@@ -57,6 +81,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        SpeedControl();
         // ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
         Debug.DrawRay(transform.position, Vector3.down,Color.red);
@@ -71,9 +96,41 @@ public class PlayerMovement : MonoBehaviour
         calculatedMoveDirection = orientation.forward * moveDirection.y + orientation.right * moveDirection.x;
         rb.AddForce(calculatedMoveDirection * moveSpeed * 10f, ForceMode.Force);
 
+        if (grounded)
+            rb.AddForce(calculatedMoveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        else if (!grounded)
+            rb.AddForce(calculatedMoveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Force);
+     
 
         // calculatedMoveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         //  rb.AddForce(calculatedMoveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
         // calculate movement direction
     }
+    private void SpeedControl() 
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x , 0f, rb.linearVelocity.z);
+
+        //limit velocity when needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
+    private void Jump()
+    {
+        // reset y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        Debug.Log("reset jump");
+
+        readyToJump = true;
+    }
+    
 }
+    
