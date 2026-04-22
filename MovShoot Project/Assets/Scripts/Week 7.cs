cb83a135c0b3492ef8ac3bbb175e0d14f52bc5f7
Dede44373 <raymonads1@gmail.com>
2026-04-22 15:14:33 +1200

    Stopping Projectile Phase stop
    ggs it works

---
+++ MovShoot Project/Assets/Scripts/Week 7.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Week7 : MonoBehaviour
+{
+    public LayerMask layerMask;
+    GameObject raycastObj;
+    Vector3 hitPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bool passedThrough = RaycastCheck();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SnapObject()
+    {
+        transform.position = hitPos;
+    }
+
+    bool RaycastCheck()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, layerMask))
+        {
+            raycastObj = hit.collider.gameObject;
+            hitPos = hit.point;
+            return false;
+        }
+        else
+        {
+            if (raycastObj != null)
+            {
+                raycastObj = null;
+                Debug.Log("passed through something");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
